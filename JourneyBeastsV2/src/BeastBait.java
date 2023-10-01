45fd08946fea860c967663420494a57b7c29870f
MoonLord2436 <97262816+MoonLord2436@users.noreply.github.com>
2023-10-01 11:16:45 -0700

    Upload Nebulosa version 2

---
+++ JourneyBeastsV2/src/BeastBait.java
@@ -0,0 +1,24 @@
+public class BeastBait {
+    private String name;
+    private int successModifier;
+    private int price;
+
+    public BeastBait(String name, int successModifier, int price) {
+        name = name;
+        successModifier = successModifier;
+        price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSuccessModifier() {
+        return successModifier;
+    }
+
+    public int price() {
+        return price;
+    }
+}
+
